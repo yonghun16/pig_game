fbca43202086ca033b2a4de89153e5594b287c98
yonghun16 <yonghun16@naver.com>
2025-03-16 06:32:06 +0900

    update

--- src/main.js
+++ src/main.js
@@ -1,24 +1,15 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+<div class="stage">
+  <div class="stage__left stage__left--selected"></div>
+  <div class="stage__right"></div>
+  <div class="buttons">
+    <div class="button__new-game">🔄 New game</div>
+  </div>
+</div>
+
+
+
+`
